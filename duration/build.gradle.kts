2376a880261ff01ea34fe02c5859e1c885183793
Gesh Markov <gesh@markov.eu>
2018-11-09 16:22:39 +0100

    Add javadoc jar to publications.

--- duration/build.gradle.kts
+++ duration/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm")
     `maven-publish`
+    id("org.jetbrains.dokka") version "0.9.17"
 }
 
 dependencies {
@@ -16,16 +17,40 @@ val sourcesJar by tasks.registering(Jar::class) {
     from(sourceSets["main"].allSource)
 }
 
+val dokka by tasks.getting(org.jetbrains.dokka.gradle.DokkaTask::class) {
+    outputFormat = "javadoc"
+    outputDirectory = "$buildDir/javadoc"
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    classifier = "javadoc"
+    from(dokka)
+}
+
 publishing {
     publications {
         register("mavenJava", MavenPublication::class) {
             from(components["java"])
             artifact(sourcesJar.get())
+            artifact(javadocJar.get())
             pom {
+                url.set("https://github.com/markov/lucid-time")
+                scm {
+                    url.set("https://github.com/markov/lucid-time")
+                    connection.set("scm:git:git://github.com/markov/lucid-time.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/markov/lucid-time.git")
+                }
                 licenses {
                     license {
                         name.set("The Apache License, Version 2.0")
                         url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("markov")
+                        name.set("Gesh Markov")
                     }
                 }
             }
