0f7fa595c50f7b0f738f891b3be78abddf08517b
Gesh Markov <gesh@markov.eu>
2018-11-09 14:04:03 +0100

    Remove support for inline classes.

--- duration/build.gradle.kts
+++ duration/build.gradle.kts
@@ -9,7 +9,3 @@ dependencies {
     testImplementation("junit:junit:4.12")
     testImplementation("org.hamcrest:hamcrest-all:1.3")
 }
-
-tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java).all {
-    kotlinOptions.freeCompilerArgs += "-XXLanguage:+InlineClasses"
-}
